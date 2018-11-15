4a7b138f8b9e7d21a5477db95b7cb0f8d346c90d
Chang Kim <cnkim@crimson.prism>
2018-11-15 12:37:35 -0800

    changed to using pandas matrix functions

--- pileupParser.py
+++ pileupParser.py
@@ -9,27 +9,27 @@ import textwrap
 from argparse import ArgumentParser
 
 def parse_args():
-	'''
-	Argument parser
-	TODO: Search for most changed base in window
-	'''
+    '''
+    Argument parser
+    TODO: Search for most changed base in window
+    '''
     parser = ArgumentParser(description="parses pileup for mismatches")
     parser.add_argument('-pileup', help='mpileup input')
     parser.add_argument('-output', help='filtered mismatches output')
-    parser.add_argument('-fasta', help='fasta output')
+    #parser.add_argument('-fasta', help='fasta output')
     return parser.parse_args()
 
-def parsePileup(pileup,output,fasta):
+def parsePileup(pileup,output):
     '''
     Parses mpileup for mismatch reads
     '''
-    pileupDf = pd.read_csv(pileup, sep='\t', header=None, names=['ref_id','ref_pos','ref_base','coverage','read_base','qual'])
-    df = df.drop(['ref_id','qual'], axis=1) # Drops useless columns
-    df = pileupDf.query('coverage >= 20') # Parses for greater than 20 depth
-    df = df[df['read_base'].str.contains('\*|\+|\-|G|C')] # Removes any pileups WITHOUT mismatches (Mismatches represented through "*, +, and -")
-    pileupList = zip(list(df['ref_pos']),list(df['coverage']),list(df['ref_base']),list(df['read_base'])) # Makes a list of tuples
-    mmList = parse(pileupList) # Returns a list of positional mismatches
-    filtered = pd.DataFrame(mmList, columns=['pos','bases','G','C','mismatches'])
+    df = pd.read_csv(pileup, sep='\t', header=None, names=['ref_id','ref_pos','ref_base','coverage','read_base','qual'])
+    df = df.drop(['qual'], axis=1) # Drops useless columns
+    df = df.query('coverage >= 20') # Parses for greater than 20 depth
+    df = df[df['read_base'].str.contains('G|C')] # Removes any pileups WITHOUT mismatches (Mismatches represented through "*, +, and -")
+    #pileupList = zip(list(df['ref_pos']),list(df['coverage']),list(df['ref_base']),list(df['read_base'])) # Makes a list of tuples
+    mmList = parse(df) # Returns a list of positional mismatches
+    filtered = pd.DataFrame(mmList, columns=['chr','pos','bases','G','C','mismatches'])
     mismatchFiltered = filtered[filtered.mismatches > .3] # Filters for greater 30% mismatch frequency
     mismatchFiltered.to_csv(output,sep='\t',index=False) # Writes positions, mismatch frequency, reference bases. TODO: Most changed base in window
     '''
@@ -48,38 +48,34 @@ def parsePileup(pileup,output,fasta):
     f.close()
     '''
 
-def newParse(pup):
+def parse(pup):
     mmList = []
-    for i in range(0,len(pup)-5,6):
+    for i in range(0,len(pup.index)-5,6):
         sliding = pup[i:i+6]
         window(sliding,mmList)
     return mmList
 
 def window(sliding,mmList):
-    totalCov = 0
+    if int(sliding.iat[0,1])+6 > int(sliding.iat[5,1]) or sliding.iat[0,0] != sliding.iat[5,0]:
+        return
+    totalCov = sliding['coverage'].sum()
+    counts = sliding['read_base'].value_counts()
+    print(counts)
     totalG = 0
     totalC = 0
+    if counts.get('G') != None:
+        totalG = counts['G']
+    if counts.get('C') != None:
+        totalC = counts['C']
     bases = ''
-    start = True
-    for pos,cov,ref,mm in sliding
-        if start:
-            startPos = pos
-            start = False
-        elif pos > startPos+6:
-            return
-        totalCov += cov
-        if ref == 'A' and 'G' in mm:
-            totalG += mm.count('G')
-        elif ref == 'T' and 'C' in mm: 
-            totalC += mm.count('C')
-        bases += ref
+    for i in range(6):
+        bases += sliding.iat[i,2]
     totalMm = (totalG+totalC)/totalCov
-    mmList.append((startPos,bases,totalG,totalC,totalMm))
-
+    mmList.append((sliding.iat[0,0],sliding.iat[0,1],bases,totalG,totalC,totalMm))
 
 def main():
     opts = parse_args()
-    parsePileup(opts.pileup,opts.output,opts.fasta)
+    parsePileup(opts.pileup,opts.output)
 
 if __name__ == '__main__':
     main()

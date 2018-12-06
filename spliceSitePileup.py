ee184c7fbc63db4a8a7dd38aa0560abd9f82e0e4
Chang Kim <cnkim@crimson.prism>
2018-12-05 17:37:56 -0800

    could optimize further

--- spliceSitePileup.py
+++ spliceSitePileup.py
@@ -1,8 +1,6 @@
 import pandas as pd
 from argparse import ArgumentParser
 
-def binarySearch():
-
 def intersect(introns,psl):
     overlaps = []
     st = {}
@@ -13,30 +11,33 @@ def intersect(introns,psl):
         for i in range(len(starts)):
             exons.append((starts[i],starts[i]+sizes[i]))
         st[row.qName] = exons
-    for row in introns.itertuples():
+    for intron in introns.itertuples():
         for pRow in psl.itertuples():
-            if row.tName in pRow.tName and row.strand == pRow.strand:
+            if intron.tName in pRow.tName and intron.strand == pRow.strand:
                 exons = st.get(pRow.qName)
-                for start,end in exons: 
-                    if row.strand == '+':
+                for start,end in exons:
+                    if start > intron.end:
+                        break 
+                    if intron.strand == '+':
                         prime = 5
                         opp = 3
                     else:
                         prime = 3
                         opp = 5
-                    if row.start < start and row.end > start:
-                        if end - row.start >= 5:
-                            overlaps.append([pRow.qName,row.tName,prime,end])
-                    if row.start < end and row.end > end:
-                        if row.end - end >= 5:
-                            overlaps.append([pRow.qName,row.tName,opp,start])
+                    if intron.start > start and intron.start < end:
+                        if start - intron.start >= 5:
+                            overlaps.append([pRow.qName,intron.tName,prime,end])
+                    if intron.end > start and intron.end < end:
+                        if end - intron.end >= 5:
+                            overlaps.append([pRow.qName,intron.tName,opp,start])
+            elif pRow.tName > intron.tName:
+                break
     return overlaps
 
 def parse_args():
     parser = ArgumentParser(description="")
     parser.add_argument('-introns', help='intron coordinates')
     parser.add_argument('-psl',help='exons')
-    parser.add_argument('-o',help='output file')
     return parser.parse_args()
 
 def main():
@@ -44,8 +45,8 @@ def main():
     introns = pd.read_csv(opts.introns,delim_whitespace=True,header=None,index_col=False,names=['tName','start','end','strand'])
     psl = pd.read_csv(opts.psl,delim_whitespace=True,header=None,index_col=False,names=['matches','misMatches','repMatches','nCount','qNumInsert','qBaseInsert','tNumInsert','tBaseInsert','strand','qName','qSize','qStart','qEnd','tName','tSize','tStart','tEnd','blockCount','blockSizes','qStarts','tStarts'])
     overlaps = intersect(introns,psl)
-    df = pd.Dataframe(overlaps,header=None)
-    df.to_csv(opts.o,sep='\t',index=False)
+    for overlap in overlaps:
+        print(overlap)
 
 if __name__ == '__main__':
     main()

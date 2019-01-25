f5af43d4d3e074b4aa24e2acba8ae08f5bec9b2f
Chang Kim <cnkim@crimson.prism>
2019-01-25 12:54:47 -0800

    combined mapper.py and link.py

--- mapper.py
+++ mapper.py
@@ -1,26 +1,35 @@
 import pandas as pd
 from argparse import ArgumentParser
 
-def mapId(splice,runs):
+def mapId(splice,runs,links):
     run2read = {}
     for read in runs.itertuples():
         run2read[read.read_id[:18]] = read.filename
+    link2full = {}
+    with open(links, "r") as ins:
+        for line in ins:
+            line = line.rstrip('\n')
+            links2full[line.split('/')[-1]] = line
     for overlap in splice.itertuples():
         reads = run2read.get(overlap.readId)
         if reads != None:
-           print(reads)
+           link = link2full.get(reads)
+           if link != None:
+               print(','.join(overlaps.readId,overlaps.ch,overlaps.end,overlaps.pos,link))
+    
 
 def parse_args():
     parser = ArgumentParser(description='')
     parser.add_argument('-splice',help='splice sites')
     parser.add_argument('-runs',help='nanopore runs')
+    parser.add_argument('-links',help='links to fast5')
     return parser.parse_args()
 
 def main():
     opts = parse_args()
-    splice = pd.read_csv(opts.splice,delimiter=',',header=None,index_col=False,names=['readId','chr','end','pos'])
+    splice = pd.read_csv(opts.splice,delimiter=',',header=None,index_col=False,names=['readId','ch','end','pos'])
     runs = pd.read_csv(opts.runs,delim_whitespace=True)
-    mapId(splice,runs)
+    mapId(splice,runs,opts.links)
 
 if __name__ == '__main__':
    main()
